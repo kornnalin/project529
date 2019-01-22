a598cb7fb795dd1ae057be59f8d3aea394ce10bf
kornnalin <firnfroy@gmail.com>
2019-01-22 18:48:33 +0700

    add image

--- image/capture_notify.py
+++ image/capture_notify.py
@@ -5,10 +5,9 @@ from time import sleep
 import datetime as dt
 import base64
 import requests
+
 from github import Github
 #import cv2
-import subprocess
-from pexpect import popen_spawn
 
 status = False
 dateTime = dt.datetime.now()
@@ -20,13 +19,12 @@ print(filepath)
 
 g = Github("7474ef783e5527b8fef335629f37dfe506897abb")
 repo = g.get_repo("kornnalin/project529")
-#print(repo.name) #project529
+print(repo.name) #project529
 
 def snap():
     print('start snap')
     camera = PiCamera()
     camera.rotation = 180
-#    camera.resolution=(330,240)
     camera.resolution=(640,480)
     camera.start_preview()
     sleep(2)
@@ -35,23 +33,29 @@ def snap():
     status = True
     
     subprocess()
+    
     line_notify_message(dates+' '+times)
     line_notify_picture("https://project529.herokuapp.com/"+filename)
  
-def subprocess():
-    user = 'kornnalin'
-    password = 'Ff012426484'
-
-    cmd = "cd /home/pi/Desktop/project529/image"
-    returned_value = subprocess.call(cmd, shell=True)
-    #cmd = "ls" 
-    #subprocess.call(cmd, shell=True)
-    cmd = 'git add '+filename
-    subprocess.call(cmd, shell=True)
-    cmd = 'git commit -m "img firn"'
-    subprocess.call(cmd, shell=True)
-    cmd = "git push -u origin master"
-    subprocess.call(cmd, shell=True)
+#def subprocess():
+#    import subprocess
+#    from pexpect import popen_spawn
+#    
+#    user = 'kornnalin'
+#    password = 'Ff012426484'
+#
+#    cmd = "cd /home/pi/Desktop/project529/image"
+#    returned_value = subprocess.call(cmd, shell=True)
+#   
+#    cmd = 'git add '+filename
+#    subprocess.call(cmd, shell=True)
+#    
+#    cmd = 'git commit -m "img firn"'
+#    subprocess.call(cmd, shell=True)
+#    
+#    cmd = "git push -u origin master"
+#    subprocess.call(cmd, shell=True)
+    
     #cmd = "kornnalin"
     #subprocess.call(cmd, shell=True)
     #cmd = "Ff012426484"
